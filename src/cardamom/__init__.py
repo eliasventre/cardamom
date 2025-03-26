65f5c0ae4e7a5ddbb9d0b900b46da73f97ef953f
Ulysse Herbach <35532736+ulysseherbach@users.noreply.github.com>
2025-03-26 19:00:42 +0100

    Small update for distribution

--- src/cardamom/__init__.py
+++ src/cardamom/__init__.py
@@ -1,14 +1,19 @@
 """
-CARDAMOM
-========
+Gene regulatory network inference from single-cell data.
 
-Gene regulatory network inference from single-cell data
--------------------------------------------------------
-
-Author: Elias Ventre
+Executable gene regulatory network (GRN) inference method adapted to
+time-stamped scRNA-seq datasets. The algorithm consists in calibrating
+the parameters of a mechanistic model of gene expression, which can then
+be simulated to reproduce the dataset used for inference.
 """
-__version__ = '1.0'
+from importlib.metadata import version as _version
+from cardamom.model import NetworkModel
 
-from .model import NetworkModel
+__all__ = [
+    'NetworkModel',
+]
 
-__all__ = ['NetworkModel']
+try:
+    __version__ = _version('cardamom')
+except Exception:
+    __version__ = 'unknown version'
